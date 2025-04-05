a5ab6d5091ab0613189469155d95013a4d7eec96
JiwonAhn203 <jahn1028@gmail.com>
2025-04-05 18:38:49 -0400

    Update models.py

--- moneyparce/accounts/models.py
+++ moneyparce/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+from datetime import datetime, timedelta
+from django.utils import timezone
 
 # Create your models here.
+class EmailVerificationCode(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_expired(self):
+        return timezone.now() > self.created_at + timedelta(minutes=10)
