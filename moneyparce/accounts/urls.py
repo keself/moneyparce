d63b1594df5d1d98822c04178a694f88dcda0b69
JiwonAhn203 <jahn1028@gmail.com>
2025-04-05 18:38:34 -0400

    Update urls.py

--- moneyparce/accounts/urls.py
+++ moneyparce/accounts/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('orders/', views.orders, name='accounts.orders'),
+    path('verify-code/', views.verify_code, name='accounts.verify_code'),
 ]
