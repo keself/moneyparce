5311c1000f46106e414a48ea6b64331750b55b6a
JiwonAhn203 <jahn1028@gmail.com>
2025-04-19 12:19:18 -0400

    Update forms.py

--- moneyparce/budgets/forms.py
+++ moneyparce/budgets/forms.py
@@ -5,6 +5,11 @@ class BudgetForm(forms.ModelForm):
     class Meta:
         model = Budget
         fields = ['name', 'amount', 'category', 'start_date', 'end_date', 'notes']
+        
+        def __init__(self, *args, **kwargs):
+            super(BudgetForm, self).__init__(*args, **kwargs)
+            self.fields['category'].initial = 'Uncategorized'
+            
         widgets = {
             'start_date': forms.DateInput(attrs={'type': 'date'}),
             'end_date': forms.DateInput(attrs={'type': 'date'}),
