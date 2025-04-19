44b7979e8c02875fddcd6f445c364a42a38d97f4
JiwonAhn203 <jahn1028@gmail.com>
2025-04-19 12:20:39 -0400

    Update forms.py

--- moneyparce/transactions/forms.py
+++ moneyparce/transactions/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from .models import Transaction
 
 class TransactionForm(forms.Form):
     amount = forms.DecimalField(max_digits=10, decimal_places=2, min_value=0)
     action = forms.ChoiceField(choices=[('add', 'Add'), ('remove', 'Remove')])
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['amount', 'action', 'category']
